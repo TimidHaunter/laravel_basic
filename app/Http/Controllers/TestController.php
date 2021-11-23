d3086a83ca2c642ef570c762130ea214a6352252
yintianxiong <yintx_129@163.com>
2021-11-24 04:11:39 +0800

    laravel dingo api 响应测试

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+//use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
 
 class TestController extends Controller
 {
     public function index()
     {
-        echo 'Test.index';
+//        return User::all();
+//        return User::find(1);
+
+        // 一个自定义消息和状态码的普通错误。
+//        return $this->response->error('自定义错误信息', 415);
+
+//        return $this->response->errorNotFound();
+
+//        return $this->response->errorInternal();
+
+//        return $this->response->errorUnauthorized();
+
+        // 自定义状态码
+        throw new \Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException('User was updated prior to your request.');
     }
 
     public function name()
