3e7202918bf989b3afee01d59ddad2cc8b1023af
yintianxiong <yintx_129@163.com>
2021-11-28 00:36:59 +0800

    laravel 用户登陆，登出，刷新token

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\BaseController;
+use App\Http\Requests\Auth\LoginRequest;
+
+class LoginController extends BaseController
+{
+    /*
+     * 用户登录
+     */
+    public function login(LoginRequest $request)
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!$token = auth('api')->attempt($credentials)){
+            return $this->response->errorUnauthorized();
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * 用户退出
+     */
+    public function logout()
+    {
+//        return 'logout';
+
+        auth('api')->logout();
+
+//        return response()->json(['message' => 'Successfully logged out']);
+
+        // 返回无状态响应
+        return $this->response->noContent();
+    }
+
+    /**
+     * 用户刷新，刷新的是 token
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken('api')->refresh();
+    }
+
+    /**
+     * 格式化返回
+     */
+    public function respondWithToken($token)
+    {
+//        return response()->json([
+//            'access_token' => $token,
+//            'token_type' => 'Bearer',
+//            'expires_in' => auth('api')->factory()->getTTL() * 60
+//        ]);
+
+        return $this->response->array([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+}
--- routes/auth.php
+++ routes/auth.php
@@ -8,8 +8,16 @@ $api->version('v1', ['middleware' => 'api.throttle', 'limit' => 60, 'expires' =>
     $api->group(['prefix' => 'auth'], function ($api){
         // 用户注册
         $api->post('register', [\App\Http\Controllers\Auth\RegisterController::class, 'store']);
-
+        // 用户登录
+        $api->post('login', [\App\Http\Controllers\Auth\LoginController::class, 'login']);
     });
 
+    // 需要登录的路由
+    $api->group(['prefix' => 'auth', 'middleware' => 'api.auth'], function($api){
+        // 用户登出
+        $api->post('logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout']);
+        // 用户刷新 token
+        $api->post('refresh', [\App\Http\Controllers\Auth\LoginController::class, 'refresh']);
+    });
 
 });
