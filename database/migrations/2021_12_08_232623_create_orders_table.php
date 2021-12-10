c2e6f7d1864d89beef23be2e22768534d89e9369
yintianxiong <yintx_129@163.com>
2021-12-10 16:18:25 +0800

    laravel 订单系统，订单，订单详情迁移表

--- database/migrations/2021_12_08_232623_create_orders_table.php
+++ database/migrations/2021_12_08_232623_create_orders_table.php
@@ -13,8 +13,30 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
+        /**
+         * order_no     订单单号
+         * user_id      下单用户
+         * amount       总金额，单位：分
+         * status       订单状态 1下单 2支付 3发货 4收货
+         * address_id   收货地址
+         * express_type 快递类型 SF YT YD
+         * express_no   快递单号
+         * pay_time     支付时间
+         * pay_type     支付类型 支付宝 微信
+         * trade_no     支付单号
+         */
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->comment('下单用户');
+            $table->string('order_no')->comment('订单单号');
+            $table->integer('amount')->comment('总金额，单位：分');
+            $table->tinyInteger('status')->default(1)->comment('订单状态 1下单 2支付 3发货 4收货');
+            $table->integer('address_id')->comment('收货地址');
+            $table->string('express_type')->comment('快递类型 SF YT YD');
+            $table->string('express_no')->comment('快递单号');
+            $table->timestamp('pay_time')->nullable()->comment('支付时间');
+            $table->string('pay_type')->nullable()->comment('支付类型 支付宝 微信');
+            $table->string('trade_no')->nullable()->comment('支付单号');
             $table->timestamps();
         });
     }
