d1c840b6f1fcfdc7f588daa8d51609117e511591
yintianxiong <yintx_129@163.com>
2021-12-12 00:14:58 +0800

    laravel 订单详情，发货

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -42,15 +42,30 @@ class OrderController extends BaseController
      */
     public function show(Order $order)
     {
-//        return $this->response->item($order, new OrderTransformer());
+        return $this->response->item($order, new OrderTransformer());
     }
 
     /**
      * PATCH
-     * 发货
+     * 发货，就是更新状态数据
      */
-    public function post()
+    public function post(Request $request, Order $order)
     {
+        // 验证提交参数，就两个字段，不需要单独的 Requests 文件
+        // 直接使用表单过滤验证
+        $request->validate([
+            'express_type' => 'required|in:SF,YT,YD',
+            'express_no'   => 'required',
+        ], [
+            'express_type.required' => '快递类型必填',
+            'express_type.in'       => '快递类型必须是SF,YT,YD',
+            'express_no.required'   => '快递单号必填',
+        ]);
 
+        $order->express_type = $request->input('express_type');
+        $order->express_no   = $request->input('express_no');
+        $order->status = 3; // 发货状态，建议设置成常量
+        $order->save();
+        return $this->response->noContent();
     }
 }
