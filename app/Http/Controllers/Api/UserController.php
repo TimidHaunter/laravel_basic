569d2b3ccfbe8c590de5ba2fe7eeeac3329e2e18
yintianxiong <yintx_129@163.com>
2021-12-22 23:41:58 +0800

    laravel 订单的数据填充

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * 显示用户个人信息
+     */
+    public function userInfo()
+    {
+
+    }
+
+
+    /**
+     * 修改信息
+     */
+    public function updateUserInfo()
+    {
+
+    }
+}
---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $orders = [
+            'user_id' => 1,
+            'order_no' => $this->faker->randomNumber(9, true),
+            'amount' => $this->faker->numberBetween(10, 9999999),
+            'status' => $this->faker->numberBetween(1, 4),
+            'address_id' => '110000',
+            'express_type' => '',
+            'express_no' => '',
+            'pay_time' => null,
+            'pay_type' => '',
+            'trade_no' => '',
+        ];
+
+        // status 1下单 2支付 3发货 4收货
+        // 只有发货以后才有 快递类型 和 快递单号
+        if ($orders['status'] >= 2) {
+            $orders['express_type'] = $this->faker->randomElement(['YD', 'SF', 'YZ', 'ST', 'HT']);
+            $orders['express_no'] = $this->faker->randomNumber(9, true);
+        }
+        // 支付以后才有 支付时间、支付类型、支付单号
+        if ($orders['status'] >= 3) {
+            $orders['pay_time'] = $this->faker->dateTimeInInterval('now', '+1 days', 'PRC');
+            $orders['pay_type'] = $this->faker->randomElement(['wx', 'zfb']);
+            $orders['trade_no'] = $this->faker->randomNumber(9, true);
+        }
+
+        return $orders;
+    }
+
+}
