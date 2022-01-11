a9222edc9750e58d20062f3a489f2c22b0c8fc7e
yintianxiong <yintx_129@163.com>
2022-01-11 23:48:26 +0800

    feat:购物车 加入购物车功能

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\BaseController;
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends BaseController
@@ -22,7 +23,23 @@ class CartController extends BaseController
      */
     public function store(Request $request)
     {
-        //
+        // 验证数据
+        // exists:goods,id 在商品表里必须存在，且必须是ID
+        $request->validate([
+            'goods_id' => 'required|exists:goods,id'
+        ],[
+            'goods_id.required' => '商品 ID不能为空',
+            'goods_id. exists' => '商品不存在',
+        ]);
+
+        Cart::create([
+            'user_id' => auth('api')->id(),
+            'goods_id' => $request->input('goods_id'),
+            'num' => $request->input('num', 1),
+        ]);
+
+
+
     }
 
     /**
--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    // 设置允许批量赋值的字段
+    protected $fillable = ['user_id', 'goods_id', 'num'];
 }
